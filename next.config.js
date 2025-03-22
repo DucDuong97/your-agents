7dd6fa92431ee5ecdfe131ea39e3b659acc2e938
DUCDUONG <duong.tracer@gmail.com>
2025-03-23 01:57:57 +0700

    debug

--- next.config.js
+++ next.config.js
@@ -10,5 +10,14 @@ module.exports = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  exclude: [
+    'app-build-manifest.json',
+    'build-manifest.json',
+    '_buildManifest.js',
+    'middleware-build-manifest.js',
+    'middleware-manifest.js',
+    '**/*.map',
+    'react-loadable-manifest.json'
+  ]
 })(nextConfig); 
