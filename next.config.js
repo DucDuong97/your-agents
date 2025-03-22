1f0b7020134aa4099b9064a95af0d1c8801b944d
DUCDUONG <duong.tracer@gmail.com>
2025-03-23 02:09:01 +0700

    debugging

--- next.config.js
+++ next.config.js
@@ -1,23 +1,28 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa');
+// Import next-pwa using ES modules
+import withPWA from 'next-pwa';
 
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
 };
 
-module.exports = withPWA({
+export default withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
   exclude: [
+    // Default exclusions
     'app-build-manifest.json',
     'build-manifest.json',
     '_buildManifest.js',
     'middleware-build-manifest.js',
     'middleware-manifest.js',
     '**/*.map',
-    'react-loadable-manifest.json'
+    'react-loadable-manifest.json',
+    // App Router specific exclusions
+    '**/*_client-reference-manifest*',
+    'server/**/*',
+    '_next/server/**/*'
   ]
 })(nextConfig); 
