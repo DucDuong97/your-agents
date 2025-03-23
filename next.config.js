244102b745071f2538cf7af62fc1f7901d021a3e
DUCDUONG <duong.tracer@gmail.com>
2025-03-23 10:48:28 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa');
+const runtimeCaching = require("next-pwa/cache");
 
 const nextConfig = {
   reactStrictMode: true,
@@ -11,18 +12,22 @@ module.exports = withPWA({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  exclude: [
-    // Default exclusions
-    'app-build-manifest.json',
-    'build-manifest.json',
-    '_buildManifest.js',
-    'middleware-build-manifest.js',
-    'middleware-manifest.js',
-    '**/*.map',
-    'react-loadable-manifest.json',
-    // App Router specific exclusions
-    '**/*_client-reference-manifest*',
-    'server/**/*',
-    '_next/server/**/*'
-  ]
+  pwa: {
+    runtimeCaching,
+    buildExcludes: [
+      // Default exclusions
+      'app-build-manifest.json',
+      'build-manifest.json',
+      '_buildManifest.js',
+      'middleware-build-manifest.js',
+      'middleware-manifest.js',
+      '**/*.map',
+      'react-loadable-manifest.json',
+      // App Router specific exclusions
+      '**/*_client-reference-manifest*',
+      'server/**/*',
+      '_next/server/**/*'
+    ]
+  }
+  
 })(nextConfig); 
